ec2063cbc604653bb51e3ef76d701369ebb8cbdf
dlrv <daniel-rocha.silva@gft.com>
2021-11-25 09:24:11 -0300

    add: Adicionado arquivo Main da aplicação

---
+++ src/main/java/com/start/controll/ControllApplication.java
@@ -0,0 +1,13 @@
+package com.start.controll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllApplication.class, args);
+	}
+
+}
