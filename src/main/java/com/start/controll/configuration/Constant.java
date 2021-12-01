0c75fc54eb5fcb6b8a42ee6fee837c177e88bcda
dlrv <daniel-rocha.silva@gft.com>
2021-12-01 17:08:56 -0300

    add: Adicionado classe com constantes

--- src/main/java/com/start/controll/configuration/Constant.java
+++ src/main/java/com/start/controll/configuration/Constant.java
@@ -1,5 +1,5 @@
 package com.start.controll.configuration;
 
 public class Constant {
-  public final static String EMPLOYEE_PHOTO_FOLDER = "storage/employee/";
+  public final static String EMPLOYEE_PHOTO_FOLDER = "storage/employee";
 }
